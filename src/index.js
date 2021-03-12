f12f7c82fb96c0462ced856dbc40d34ff4fc463f
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-03-12 14:48:54 -0300

    challenge finished

--- src/index.js
+++ src/index.js
@@ -10,19 +10,75 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(item => item.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "Error"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const amount = user.todos.length;
+
+  if(amount >= 10 && !user.pro) {
+    return response.status(403).json({ error: "Error"});
+  }
+
+  return next();
+  
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(item => item.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "Error"});
+  }
+
+  if(!validate(id)) {
+    return response.status(400).json({ error: "Error"});
+  }
+
+  const todo = user.todos.find(item => item.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: "Error"});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
+/*
+Esse middleware possui um funcionamento semelhante ao middleware checksExistsUserAccount 
+mas a busca pelo usuário deve ser feita através do id de um usuário passado por parâmetro na rota. 
+Caso o usuário tenha sido encontrado, o mesmo deve ser repassado para dentro do request.user e a função next deve ser chamada.
+*/
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(item => item.id === id);
+
+  if(!user) {
+    return response.status(404).json({ error: "Error"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
